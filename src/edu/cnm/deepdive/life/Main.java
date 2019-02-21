818f68bef7375bddcbdc70a72e97754d4f297d88
Nick Bennett <nick@nickbenn.com>
2019-02-21 12:01:30 -0700

    Initial commit

---
+++ src/edu/cnm/deepdive/life/Main.java
@@ -0,0 +1,34 @@
+package edu.cnm.deepdive.life;
+
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+  private static final String RESOURCE_DIR = "res/";
+  private static final String BUNDLE_NAME = RESOURCE_DIR + "ui-strings";
+  private static final String LAYOUT_NAME = RESOURCE_DIR + "life.fxml";
+  private static final String WINDOW_TITLE_KEY = "title";
+
+  public static void main(String[] args) {
+    launch(args);
+  }
+
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    ClassLoader classLoader = getClass().getClassLoader();
+    ResourceBundle bundle = ResourceBundle.getBundle(BUNDLE_NAME);
+    FXMLLoader fxmlLoader = new FXMLLoader(classLoader.getResource(LAYOUT_NAME), bundle);
+    Parent parent = fxmlLoader.load();
+    Scene scene = new Scene(parent);
+    primaryStage.setScene(scene);
+    primaryStage.setTitle(bundle.getString(WINDOW_TITLE_KEY));
+    primaryStage.setResizable(false);
+    primaryStage.show();
+  }
+
+}
